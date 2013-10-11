f6088cb4981c6e61613372baceeac2850cade1fc
Povilas Panavas <povilas.panavas@gmail.com>
2013-10-11 22:37:06 +0100

    Moved comments to more appropriate place. Also, added casesensitive parameter.

--- StringFilter/StringFilter.cs
+++ StringFilter/StringFilter.cs
@@ -1,30 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 
 namespace StringFilter
 {
+    /// <summary>
+    /// The assignment wasn't clear enough as most often happens with real requirements.
+    /// Firstly, "NUnit" is third party library in my point of view (open source library)
+    /// and shouldn't be included according to the assesment requirements. So, here is the link
+    /// with included NUnit where you could just launch solution, compile and run tests:
+    /// git clone https://github.com/povilaspanavas/HireFSOStringSearch.git
+    /// Secondly, there isn't data about incoming string lists. For example, if there
+    /// is lots of small strings, but small count of strings with length of 6, we
+    /// could do different search (cycle trough only strings of length 6 ant try to 
+    /// disassemble them, then search for disassembled parts).
+    /// If that would be too slow, we could have dictionaries, where key would be first
+    /// letter, and value hashset containing all the words starting with that letter.
+    /// Thirdly, no mentioning if data is case sensitive? Just in case, I've made a 
+    /// parrameter for that.
+    /// </summary>
     public class StringFilter
     {
+        private readonly bool _caseSensitive = false;
+
+        public StringFilter(bool caseSensitive = false)
+        {
+            _caseSensitive = caseSensitive;
+        }
         /// <summary>
-        /// The assignment wasn't clear enough as most often happens with real requirements.
-        /// Firstly, "NUnit" is third party library in my point of view (open source library)
-        /// and shouldn't be included. Strainge, isn't it?.. So, here is the link
-        /// with included NUnit where you could just launch solution, compile and run tests:
-        /// git clone https://github.com/povilaspanavas/HireFSOStringSearch.git
-        /// Secondly, there isn't data about incoming string lists. For example, if there
-        /// is lots of small strings, but small count of strings with length of 6, we
-        /// could do different search (cycle trough only strings of length 6 ant try to 
-        /// disassemble them, then search for disassembled parts).
-        /// If that would be too slow, we could have dictionaries, where key would be first
-        /// letter, and value hashset containing all the words starting with that letter.
-        /// Thirdly, no mentioning if data is case sensitive? Current implementation is,
-        /// but it's easy to change.
+        /// Finds strings of length 6 which are concatenated from other two strings in list.
         /// </summary>
         /// <param name="strList"></param>
         /// <returns></returns>
         public List<string> Filter(List<string> strList)
         {
+            if (_caseSensitive)
+                strList = new List<string>(strList.Select(s => s.ToLower()));
+
             if (strList == null)
                 throw new ArgumentNullException("strList");
             if (strList.Count == 0)
