dc20161d81488a2374c9c2fc4ba9d8253c8c0a96
Povilas Panavas <povilas.panavas@gmail.com>
2013-10-11 18:13:56 +0100

    First two tests passes. Also added NUnit.Framework.dll

--- StringFilter/StringFilter.cs
+++ StringFilter/StringFilter.cs
@@ -10,6 +10,10 @@ namespace StringFilter
     {
         public List<string> Filter(List<string> strList)
         {
+            if (strList == null)
+                throw new ArgumentNullException("strList");
+            if (strList.Count == 0)
+                return new List<string>();
             var hashSet = new HashSet<string>(); // lists are slow for lots of data
             var resultHashSet = new HashSet<string>(); // results
             foreach (var str in strList)
@@ -20,8 +24,9 @@ namespace StringFilter
             {
                 if (strList[i].Length >= 6) // technically it wouldn't be two strings if the first is length of 6
                     continue;
-
-                for (int j = 1; j < strList.Count; j++)
+                // TODO ilgis turi b8ti  6
+                // TODO tikrinti i6 abiej7 galu 0 + 1 ir 1 + 0
+                for (int j = i + 1; j < strList.Count; j++)
                 {
                     var concatStr= strList[i] + strList[j];
                     if (concatStr.Length > 6) // we are limited to 6 symbols
---
+++ StringFilter/Tests/TestStringFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringFilter.Tests
+{
+    [TestFixture]
+    public class TestStringFilter
+    {
+        private StringFilter _stringFilter = new StringFilter();
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullParameter()
+        {
+            _stringFilter.Filter(null);
+        }
+
+        [Test]
+        public void TestEmptyList()
+        {
+            Assert.AreEqual(0, _stringFilter.Filter(new List<string>()).Count);
+        }
+    }
+}
